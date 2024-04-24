486795535c59a1d4a8855c1c65195b0c661414ca
santimuller45 <santiagomuller45@gmail.com>
2024-04-24 20:19:44 -0300

    24-04-24

---
+++ src/helper/helperCategory.js
@@ -0,0 +1,107 @@
+const category = {
+    "QUIMICOS Y DILUYENTES": {
+        subRubros: [
+            { name: 'QUIMICOS PARA FERRETERIAS' },
+            { name: 'DILUYENTES' },
+            { name: 'ACEITE DE LINO Y ACEITE LUBRICANTE' }
+        ]
+    },
+    "PINTURAS": {
+        subRubros: [
+            { name: 'CONVERTIDOR Y ANTIOXIDOS PARA SUPERFICIES' },
+            { name: 'FONDOS Y FIJADORES DE SUPERFICIES' },
+            { name: 'ENTONADOR UNIVERSAL' },
+            { name: 'ESMALTE SINTETICO NETCOLOR' },
+            { name: 'CONVERTIDOR + ESMALTE NETCOLOR' },
+            { name: 'ESMALTE SATINADOR NETCOLOR' },
+            { name: 'ESMALTE SINTETICO MATE NETCOLOR' },
+            { name: 'BARNIZ MARINO NETCOLOR' },
+            { name: 'TINTA PARA BARNIZ NETCOLOR' },
+            { name: 'PINTURAS EN AEROSOL' },
+            { name: 'PINTURA ASFALTICA Y EPOXI' },
+            { name: 'CAL DE BLANQUEO' },
+            { name: 'PINTURA EN POLVO' },
+            { name: 'LATEX FRENTES NETCOLOR' },
+            { name: 'FRENTES FLEX NETCOLOR' },
+            { name: 'TEXTUDECOR VENIER (revear)' },
+            { name: 'TECHOS FLEX NETCOLOR' },
+            { name: 'MEMBRANA EN PASTA MI COLOR' },
+            { name: 'CIELORRASO NETCOLOR' },
+            { name: 'PREMIUM PLUS SATINADO NETCOLOR Lavable+Limpiable' },
+            { name: 'PREMIUM BASE TRANSPARENTE NETCOLOR Lavable+Limpiable' },
+            { name: 'PREMIUM INTERIOR LAVABLE NETCOLOR' },
+            { name: 'LATEX INTERIOR- EXTERIOR NETCOLOR' },
+            { name: 'LATEX AMATEUR EXTERIOR' },
+            { name: 'LATEX AMATEUR INTERIOR' },
+            { name: 'PINTURA PARA PIZARRON Y ALTA TEMPERATURA' }
+        ]
+    },
+    "ACCESORIOS PARA PINTURA": {
+        subRubros: [
+            { name: 'PINCELES EL COATI' },
+            { name: 'PINCELETAS EL COATI/LUMAR' },
+            { name: 'RODILLOS EL COATI/LUMAR' },
+            { name: 'MINI RODILLOS EL COATI' },
+            { name: 'BANDEJAS DE PINTOR Y ACCESORIOS' },
+            { name: 'LIJAS Y ABRASIVOS' }
+        ]
+    },
+    "AUTOMOTOR": {
+        subRubros: [
+            { name: 'LUSTRES Y MASILLAS' }
+        ]
+    },
+    "POLIETILENO Y AGROTILENO": {
+        subRubros: [
+            { name: 'NYLON NEGRO' },
+            { name: 'NYLON CRISTAL' }
+        ]
+    },
+    "FERRETERIA": {
+        subRubros: [
+            { name: 'COBERTORES PARA CAÑOS' },
+            { name: 'TANZAS' },
+            { name: 'PRECINTOS' },
+            { name: 'TARUGOS DE NYLON' },
+            { name: 'TORNILLOS' }
+        ]
+    },
+    "ARTICULOS DE LIMPIEZA Y VARIOS": {
+        subRubros: [
+            { name: 'ESCOBAS Y ACCESORIOS DE LIMPIEZA' }
+        ]
+    },
+    "PRODUCTOS PARA PILETAS": {
+        subRubros: [
+            { name: 'GRANULADOS Y LIQUIDOS' }
+        ]
+    },
+    "INSECTICIDAS Y RATICIDAS": {
+        subRubros: [
+            { name: 'INSECTICIDAS Y RATICIDAS'}
+        ]
+    },
+    "ARTICULOS PARA LA CONSTRUCCION": {
+        subRubros: [
+            { name: 'KLAUKOL' },
+            { name: 'SIKA' },
+            { name: 'SUPERTECH' },
+            { name: 'FERRITES' },
+            { name: 'CAL VIVA - YESO PARIS - CEMENTO' }
+        ]
+    },
+    "ZINGUERIA": {
+        subRubros: [
+            { name: 'CAÑOS Y ACCESORIOS GALVANIZADOS P/VENTILACION' },
+            { name: 'REDUCCIONES GALVANIZADAS' },
+            { name: 'CAÑO Y ACCESORIO PARA SALAMANDRA' },
+            { name: 'EXTRACTORES EOLICOS DE ALUMINIO' },
+            { name: 'GRAMPAS OMEGA' },
+            { name: 'CANALETAS Y ACCESORIOS PARA TECHOS' },
+            { name: 'CABALLETES / CUMBRERAS PARA TECHOS'},
+            { name: 'CENEFAS PARA TECHOS'}
+        ]
+    }
+};
+
+module.exports = category;
