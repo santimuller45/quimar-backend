cbc3417cb1dfe4b21533d325f335d2dc1981eeb7
santi <santiagomuller45@gmail.com>
2024-11-02 20:11:55 -0300

    02-11-24 agregada las rutas para el deploy en railway

--- src/db.js
+++ src/db.js
@@ -2,15 +2,32 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
-const sequelize = new Sequelize(
-   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-   {
-      logging: false, // set to console.log to see the raw SQL queries
-      native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// VERSION LOCAL
+// const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+
+// const sequelize = new Sequelize(
+   //    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+   //    {
+      //       logging: false, // set to console.log to see the raw SQL queries
+      //       native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+      //    }
+      // );
+      
+// VERSION RAILWAY
+const { DB_URL_RAILWAY } = process.env;
+
+const sequelize = new Sequelize(DB_URL_RAILWAY, {
+   logging: false,
+   dialect: 'postgres',
+   dialectOptions: {
+       ssl: {
+           require: true,
+           rejectUnauthorized: false // Para evitar errores de SSL
+       }
    }
-);
+});
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
